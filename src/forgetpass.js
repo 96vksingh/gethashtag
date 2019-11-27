ac05148d81b5d97e3cf84b7575543ac536ceb3e0
Vishal KumarSingh <01vksingh@gmail.com>
2019-11-27 11:25:17 +0530

    initial

---
+++ src/forgetpass.js
@@ -0,0 +1,7 @@
+import React from 'react'
+class Forget extends React.Component {
+  render() {
+    return <h1>forget pass</h1>
+  }
+}
+export default Forget
---
+++ src/signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+class Signup extends React.Component {
+  render() {
+    return (<h1>SIGN UP!</h1>
+
+
+    )
+  }
+}
+export default Signup
